625fc136aedbd6704ace4324371cd16486ef4cb1
mugazambis <mugazambis@cmc304-01.mathcs.carleton.edu>
2017-01-27 15:31:28 -0600

    120 Version

---
+++ 120Version/110triangle.c
@@ -0,0 +1,173 @@
+/*
+@ Author:  Sabastian Mugazambi & Tore Banta
+@ Date: 01/07/2017
+This files includes the function that renders a triangle by rasterisation. It
+also handles triangle coodinates
+given in any order.
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include "000pixel.h"
+
+
+
+/*
+@function triRender
+@param (double a0,double a1,double b0,double b1,double c0,double c1,double
+r,double g,double b), where
+a0,...,b1 are coodinates of the triangle and r,g,b represent the color of the
+triangle to be drawn.
+@purpose This function first normalises whatever triangle coodinates it is
+given. It then renders
+the triangle according to the equations of lines. It also has some cotrol logic
+for different triangle shapes
+and situations.
+*/
+
+void hiddenRender(renRenderer *ren, double unif[], texTexture *tex[], double a[],
+        double b[], double c[]) {
+
+  //do shiftIsDown
+  double bminusa[2], cminusa[2], m[2][2], mInv[2][2], pq[2];
+  vecSubtract(2, b, a, bminusa);
+  vecSubtract(2, c, a, cminusa);
+  mat22Columns(bminusa, cminusa, m);
+  mat22Columns(bminusa, cminusa, mInv);
+  if (mat22Invert(m, mInv) < 0.0) {
+    return;
+  }
+
+  double xleft, yleft, xmid, ymid, xright, yright;
+
+  xleft = a[renVARYX];
+  yleft = a[renVARYY];
+
+  if (b[0] <= c[0]) {
+    xmid = b[renVARYX];
+    ymid = b[renVARYY];
+
+    xright = c[renVARYX];
+    yright = c[renVARYY];
+  } else {
+    xmid = c[renVARYX];
+    ymid = c[renVARYY];
+
+    xright = b[renVARYX];
+    yright = b[renVARYY];
+  }
+
+  // Now draw the first half of the triangle
+  // For every x coodinate between left most point to mid point
+  //printf("rght = %d , ceil(xleft) = %d , floor(xmid) = %d\n", rght,(int)ceil(xleft), (int)floor(xmid));
+  for (int i = (int)ceil(xleft); i <= (int)floor(xmid); i++) {
+    double ylow, yhigh;
+    int y_Top,y_Bottom;
+
+    ylow = yleft + (((yright - yleft) / (xright - xleft)) * (i - xleft));
+    yhigh = yleft + (((ymid - yleft) / (xmid - xleft)) * (i - xleft));
+    y_Bottom= ceil(fmin(ylow,yhigh));
+    y_Top = floor(fmax(yhigh,ylow));
+
+
+    // draw the triangle
+    for (int j = y_Bottom; j <= y_Top; j++) {
+      double xminusa[2], x[2];
+      x[0] = i;
+      x[1] = j;
+
+      vecSubtract(2, x, a, xminusa);
+
+      mat221Multiply(mInv, xminusa, pq);
+      double p = pq[0];
+      double q = pq[1];
+
+      double betaminusalpha[renVARYDIMBOUND], gammaminusalpha[renVARYDIMBOUND], vary[renVARYDIMBOUND];
+
+      vecSubtract(ren->varyDim, b, a, betaminusalpha);
+      vecSubtract(ren->varyDim, c, a, gammaminusalpha);
+
+      double pvec[renVARYDIMBOUND], qvec[renVARYDIMBOUND], pplusqvec[renVARYDIMBOUND];
+
+      vecScale(ren->varyDim, p, betaminusalpha, pvec);
+      vecScale(ren->varyDim, q, gammaminusalpha, qvec);
+
+      vecAdd(ren->varyDim, pvec, qvec, pplusqvec);
+      vecAdd(ren->varyDim, pplusqvec, a, vary);
+
+      double rgbz[4];
+      if(vary[renVARYZ] > depthGetZ(ren->depth,i,j)){
+        ren->colorPixel(ren, unif, tex, vary, rgbz);
+        pixSetRGB(i, j, rgbz[0], rgbz[1], rgbz[2]);
+        depthSetZ(ren->depth,i,j,vary[renVARYZ]);
+      }
+    }
+}
+
+  // from xmid to xright
+  //printf("ceil(xmid) = %d, floor(xright) = %d\n", (int)ceil(xmid), (int)floor(xright));
+  for (int i = (int)ceil(xmid); i <= (int)floor(xright); i++) {
+    double ylow, yhigh;
+    int y_Top,y_Bottom;
+
+    ylow = yleft + (((yright - yleft) / (xright - xleft)) * (i - xleft));
+    yhigh = ymid + (((yright - ymid) / (xright - xmid)) * (i - xmid));
+    y_Bottom= ceil(fmin(ylow,yhigh));
+    y_Top = floor(fmax(yhigh,ylow));
+
+    double xminusa[2], x[2];
+
+    for (int j = y_Bottom ; j <= y_Top; j++) {
+      x[0] = i;
+      x[1] = j;
+
+      vecSubtract(2, x, a, xminusa);
+
+      mat221Multiply(mInv, xminusa, pq);
+      double p = pq[0];
+      double q = pq[1];
+
+      double betaminusalpha[renVARYDIMBOUND], gammaminusalpha[renVARYDIMBOUND], vary[renVARYDIMBOUND];
+
+      vecSubtract(ren->varyDim, b, a, betaminusalpha);
+      vecSubtract(ren->varyDim, c, a, gammaminusalpha);
+
+      double pvec[renVARYDIMBOUND], qvec[renVARYDIMBOUND], pplusqvec[renVARYDIMBOUND];
+
+      vecScale(ren->varyDim, p, betaminusalpha, pvec);
+      vecScale(ren->varyDim, q, gammaminusalpha, qvec);
+
+      vecAdd(ren->varyDim, pvec, qvec, pplusqvec);
+      vecAdd(ren->varyDim, pplusqvec, a, vary);
+
+
+      double rgbz[4];
+      //printf("[%f,%f,%f]\n", vary[0], vary[1], vary[2]);
+      if(vary[renVARYZ] > depthGetZ(ren->depth,i,j)){
+        ren->colorPixel(ren, unif, tex, vary, rgbz);
+        pixSetRGB(i, j, rgbz[0], rgbz[1], rgbz[2]);
+        depthSetZ(ren->depth,i,j,vary[renVARYZ]);
+      }
+
+    }
+  }
+}
+
+
+void triRender(renRenderer *ren, double unif[], texTexture *tex[], double a[],
+        double b[], double c[]) {
+      // Do the normalisation of the triangle by reassigning triangle coodinate
+      // values and attributes
+
+      if (a[0] <= b[0] && a[0] <= c[0]) {
+        hiddenRender(ren, unif, tex, a, b, c);
+      }
+
+      else if (b[0] <= a[0] && b[0] <= c[0]) {
+        hiddenRender(ren, unif, tex, b, c, a);
+      }
+
+      else if (c[0] <= b[0] && c[0] <= a[0]) {
+        hiddenRender(ren, unif, tex, c, a, b);
+      }
+}
--- 120Version/120mainCamera.c
+++ 120Version/120mainCamera.c
@@ -3,7 +3,7 @@
 @ Date: 01/07/2017
 This files includes the main function that test the 020triangle.c rasterizing
 script.
-Run the script like so  clang 110mainDepth.c 000pixel.o -lglfw -framework OpenGL
+Run the script like so  clang 120mainCamera.c 000pixel.o -lglfw -framework OpenGL
 */
 
 #include <stdio.h>
@@ -16,10 +16,16 @@ Run the script like so  clang 110mainDepth.c 000pixel.o -lglfw -framework OpenGL
 #include "040texture.c"
 #include "110depth.c"
 
+#define GLFW_KEY_ENTER 257
+#define GLFW_KEY_RIGHT 262
+#define GLFW_KEY_LEFT 263
+#define GLFW_KEY_DOWN 264
+#define GLFW_KEY_UP 265
+
 #define renVARYDIMBOUND 16
 #define renVERTNUMBOUND 300
 
-#include "110renderer.c"
+#include "120renderer.c"
 
 #define renVARYX 0
 #define renVARYY 1
@@ -42,6 +48,7 @@ Run the script like so  clang 110mainDepth.c 000pixel.o -lglfw -framework OpenGL
 #define renUNIFTRANSY 4
 #define renUNIFTRANSZ 5
 #define renUNIFISOMETRY 6
+#define renUNIFVIEWING 22
 
 double x_val = 0.0;
 #define renATTRX 0
@@ -53,25 +60,37 @@ double x_val = 0.0;
 #define renATTRG 6
 #define renATTRB 7
 
-double unif[22] = {0.0,1.0,1.6,250.0,250.0,0.0, 1.0,0.0,0.0,0.0,
-                                             0.0,1.0,0.0,0.0,
-                                             0.0,0.0,1.0,0.0,
-                                             0.0,0.0,0.0,1.0};
+double cam[2] = {M_PI/2,-1*M_PI/2};
 
-double unif2[22] = {0.0,0.0,0.0,0.0,0.0,50.0, 1.0,0.0,0.0,0.0,
-                                            0.0,1.0,0.0,0.0,
-                                            0.0,0.0,1.0,0.0,
-                                            0.0,0.0,0.0,1.0};
+double target[3] = {-100.0, -100.0, -100.0};
+
+double unif[38] = {0.0,1.0,1.6,0.0,0.0,0.0,     1.0,0.0,0.0,0.0,
+                                                0.0,1.0,0.0,0.0,
+                                                0.0,0.0,1.0,0.0,
+                                                0.0,0.0,0.0,1.0, 0.0,0.0,0.0,0.0,
+                                                                 0.0,0.0,0.0,0.0,
+                                                                 0.0,0.0,0.0,0.0,
+                                                                 0.0,0.0,0.0,0.0};
+
+double unif2[38] = {0.0,0.0,0.0,0.0,0.0,50.0, 1.0,0.0,0.0,0.0,
+                                              0.0,1.0,0.0,0.0,
+                                              0.0,0.0,1.0,0.0,
+                                              0.0,0.0,0.0,1.0, 0.0,0.0,0.0,0.0,
+                                                               0.0,0.0,0.0,0.0,
+                                                               0.0,0.0,0.0,0.0,
+                                                               0.0,0.0,0.0,0.0};
 /* Writes the vary vector, based on the other parameters. */
 void transformVertex(renRenderer *ren, double unif[], double attr[],
         double vary[]) {
     /* For now, just copy attr to varying. Baby steps. */
     double attrXYZvec[4] = {attr[renATTRX],attr[renATTRY],attr[renATTRZ],1};
     double RtimesXYZvec[4];
+    double MtimesRvec[4];
     mat441Multiply((double(*)[4])(&unif[renUNIFISOMETRY]),attrXYZvec,RtimesXYZvec);
-    vary[renVARYX] = RtimesXYZvec[0];
-    vary[renVARYY] = RtimesXYZvec[1];
-    vary[renVARYZ] = RtimesXYZvec[2];
+    mat441Multiply((double(*)[4])(&unif[renUNIFVIEWING]),RtimesXYZvec,MtimesRvec);
+    vary[renVARYX] = MtimesRvec[0];
+    vary[renVARYY] = MtimesRvec[1];
+    vary[renVARYZ] = MtimesRvec[2];
     vary[renVARYS] = attr[renATTRS];
     vary[renVARYT] = attr[renATTRT];
 }
@@ -87,6 +106,8 @@ void updateUniform(renRenderer *ren, double unif[], double unifParent[]) {
 
   vec3Spherical(1.0,unif[renUNIFPHI],unif[renUNIFTHETA],u);
   mat33AngleAxisRotation(unif[renUNIFRHO],u,rot);
+
+  mat44Copy(ren->viewing, (double(*)[4])(&unif[renUNIFVIEWING]));
 
   if (unifParent == NULL) {
 
@@ -139,7 +160,7 @@ depthBuffer dep;
 
 void handleKeyUp(int button, int shiftIsDown, int controlIsDown,
                  int altOptionIsDown, int superCommandIsDown) {
-  if (button == 257) {
+  if (button == GLFW_KEY_ENTER) {
     if (filter == 0) {
       texSetFiltering(tex[0], texQUADRATIC);
       filter = 1;
@@ -147,11 +168,36 @@ void handleKeyUp(int button, int shiftIsDown, int controlIsDown,
       texSetFiltering(tex[0], texNEAREST);
       filter = 0;
     }
+  } else if (button == GLFW_KEY_UP) {
+    if (cam[0] + 0.05 > M_PI) {
+      cam[0] = 0.05;
+    } else {
+      cam[0] = cam[0] + 0.05;
+    }
+  } else if (button == GLFW_KEY_DOWN) {
+    if (cam[0] - 0.05 < 0.0) {
+      cam[0] = M_PI - 0.05;
+    } else {
+      cam[0] = cam[0] - 0.05;
+    }
+  } else if (button == GLFW_KEY_LEFT) {
+    if (cam[1] - 0.05 < (-1*M_PI)) {
+      cam[1] = M_PI - 0.05;
+    } else {
+      cam[1] = cam[1] - 0.05;
+    }
+  } else if (button == GLFW_KEY_RIGHT) {
+    if (cam[1] + 0.05 > M_PI) {
+      cam[1] = (-1*M_PI) + 0.05;
+    } else {
+      cam[1] = cam[1] + 0.05;
+    }
   }
 }
 
 
 void draw() {
+  renUpdateViewing(&ren);
   depthClearZs(&dep,-1000);
   pixClearRGB(0.0,0.0,0.0);
   sceneRender(&scen0,&ren,NULL);
@@ -161,6 +207,7 @@ void handleRotation() {
   //scen0.unif[renUNIFRHO] = scen0.unif[renUNIFRHO] + 0.05;
   //scen0.unif[renUNIFPHI] = scen0.unif[renUNIFPHI] + 0.05;
   scen0.unif[renUNIFRHO] = scen0.unif[renUNIFRHO] + 0.05;
+  renLookAt(&ren, target, 150.0, cam[0], cam[1]);
   //vec3Spherical(1.0,unif[renUNIFPHI],unif[renUNIFTHETA],u);
   //mat33AngleAxisRotation(unif[renUNIFRHO],u,rot);
 
@@ -169,6 +216,7 @@ void handleRotation() {
 void handleTimeStep(double oldTime, double newTime) {
   if (floor(newTime) - floor(oldTime) >= 1.0)
     printf("handleTimeStep: %f frames/sec\n", 1.0 / (newTime - oldTime));
+    //printf("[%f, %f]\n", cam[0], cam[1]);
     handleRotation();
     draw();
 }
@@ -195,7 +243,7 @@ int main(void) {
     ren.attrDim = 8;
     ren.varyDim = 4;
     ren.texNum = 1;
-    ren.unifDim = 22;
+    ren.unifDim = 38;
     ren.colorPixel = colorPixel;
     ren.transformVertex = transformVertex;
     ren.updateUniform = updateUniform;
@@ -207,7 +255,7 @@ int main(void) {
     pixSetTimeStepHandler(handleTimeStep);
     pixSetKeyUpHandler(handleKeyUp);
 
-    meshInitializeBox(&mesh0, 0.0, 150.0, 0.0, 150.0, 50.0, 150.0);
+    meshInitializeBox(&mesh0, 0.0, 150.0, 0.0, 150.0, 0.0, 50.0);
     meshInitializeSphere(&mesh1, 40, 20, 20);
 
     sceneInitialize(&scen0,&ren,unif,tex,&mesh0,NULL,NULL);
@@ -218,7 +266,7 @@ int main(void) {
     sceneSetUniform(&scen1,&ren,unif2);
     sceneAddChild(&scen0,&scen1);
 
-
+    renLookAt(&ren, target, 150.0, cam[0], cam[1]);
 
     draw();
     pixRun();
