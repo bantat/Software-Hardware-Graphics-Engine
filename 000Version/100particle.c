f34c380f161204f676a3736a84d9bf9314ad0f32
Sabastian Mugazambi <mugazambi@gmail.com>
2017-03-15 13:30:00 -0500

    Clean up

---
+++ 000Version/100particle.c
@@ -0,0 +1,38 @@
+
+/* represents a particle strcut or class with amethods related
+*/
+
+// CPU representation of a particle
+struct Particle{
+	glm::vec3 pos, speed;
+	unsigned char r,g,b,a; // Color
+	float size, angle, weight;
+	float life; // Remaining life of the particle. if < 0 : dead and unused.
+
+};
+
+const int MaxParticles = 100000;
+Particle ParticlesContainer[MaxParticles];
+
+int LastUsedParticle = 0;
+
+// Finds a Particle in ParticlesContainer which isn't used yet.
+// (i.e. life < 0);
+int FindUnusedParticle(){
+
+    for(int i=LastUsedParticle; i<MaxParticles; i++){
+        if (ParticlesContainer[i].life < 0){
+            LastUsedParticle = i;
+            return i;
+        }
+    }
+
+    for(int i=0; i<LastUsedParticle; i++){
+        if (ParticlesContainer[i].life < 0){
+            LastUsedParticle = i;
+            return i;
+        }
+    }
+
+    return 0; // All particles are taken, override the first one
+}
